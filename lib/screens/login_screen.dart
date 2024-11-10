afbcd910c42aa215aa47143bacf3ab79830fae70
NexusGk Softwares <nexusgksoftwares@gmail.com>
2024-11-10 11:24:34 +0300

    "Refactor main.dart to use AdminPanelApp and remove MyHomePage and _MyHomePageState; update widget_test.dart to match changes"

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,47 @@
+// login_screen.dart
+import 'package:flutter/material.dart';
+import 'dashboard_screen.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Admin Login'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const TextField(
+              decoration: InputDecoration(
+                labelText: 'Username',
+              ),
+            ),
+            const SizedBox(height: 10),
+            const TextField(
+              decoration: InputDecoration(
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Simulate login success and navigate to the dashboard
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => DashboardScreen()),
+                );
+              },
+              child: const Text('Login'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
